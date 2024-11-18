622551474517d25ec84eb78f062df85447b560fe
wei30172 <claire610323@gmail.com>
2024-11-17 17:21:00 -0800

    zoom 3d preview

--- src/app/features/obstacle-testing/obstacle-editor.component.ts
+++ src/app/features/obstacle-testing/obstacle-editor.component.ts
@@ -6,5 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./obstacle-editor.component.scss']
 })
 export class ObstacleEditorComponent implements OnInit {
+  isBabylonSectionExpanded = true;
+
   ngOnInit(): void {}
+
+  zoomBabylonSection(): void {
+    this.isBabylonSectionExpanded = !this.isBabylonSectionExpanded;
+  }
 }
